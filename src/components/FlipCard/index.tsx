cf86290adada265eaa7a1130d7ece68202605e44
Mathijs Rutgers <mathijs.rutgers@tradecast.eu>
2024-05-10 18:49:04 +0200

    Make default card sizes 1.5 larger

--- src/components/FlipCard/index.tsx
+++ src/components/FlipCard/index.tsx
@@ -18,7 +18,8 @@ export default function FlipCard({ cardSide, front, back, type = 'poker' }: IPro
         <div
             className={classNames(
                 'relative group [perspective:1000px]',
-                type === 'tarot' ? `w-[70mm] h-[120mm]` : `w-[64mm] h-[89mm]`,
+                // Size x 1.5
+                type === 'tarot' ? `w-[105mm] h-[180mm]` : `w-[96mm] h-[133mm]`,
             )}
         >
             <div
